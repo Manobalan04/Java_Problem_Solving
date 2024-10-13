202da9e4e44f39f33c2e1e99455f78a8c76b846a
Manobalan04 <manobalans2001@outlook.com>
2024-10-13 20:07:49 +0530

    first commit

---
+++ src/Problems/StringToCharArray.java
@@ -0,0 +1,23 @@
+package Problems;
+
+public class StringToCharArray {
+
+	public static void main(String [] args) {
+		
+		String name="Hello";
+		char []arr= name.toCharArray();
+		
+		for(char i: arr) {
+			System.out.println(i);
+		}
+		
+//		String name="Hello";
+//		String []arr= name.toCharArray();
+//		
+//		for(String i: arr) {
+//			System.out.println(i);
+//		}
+		
+	}
+	
+}
